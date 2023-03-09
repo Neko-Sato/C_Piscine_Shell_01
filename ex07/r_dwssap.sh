25b65113de765d6fb5558f2b93aa1ab767a5c3d0
hshimizu <hisatosi35350@gmail.com>
2023-03-09 12:19:29 +0900

    ex07

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,3 +1,1 @@
-FT_LINE1=7
-FT_LINE2=15
 cat /etc/passwd | awk -F ':' '!/^#/ { print $1 }' | awk 'NR%2==0' | rev | sort -r | awk "$FT_LINE1<=NR&&NR<=$FT_LINE2" | awk 'NR==1 { printf("%s", $0) } NR>1 { printf(", %s", $0) } END { printf(".")}'
